0201d06ffac1695fa186dfe834bbe962c6dbd240
J. Klesley <klesley.pfeitosa@gmail.com>
2025-05-05 17:35:29 -0300

    feat: implementa a tela que exibe todas as músicas e adiciona propriedades ao componente Music

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,56 @@
-import { View, StyleSheet,Text, SafeAreaView} from "react-native";
+import { useState, useEffect } from "react";
+import { View, StyleSheet,Text, SafeAreaView, FlatList, TouchableOpacity } from "react-native";
+import * as MediaLibrary from "expo-media-library";
+import type { PagedInfo, Asset } from "expo-media-library";
+import Music from "@/components/Music";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 //Primeira tela do App
 export default function Index() {
+  const [musics, setMusics] = useState<PagedInfo<Asset>>();
+  const [responsePermissions, requestPermissions] = MediaLibrary.usePermissions();
+
+  const getPermissions = async () => {
+    if(responsePermissions?.status !== "granted"){
+      await requestPermissions();
+    }
+  }
+
+  const getMusics = async () => {
+    let foundMusics = await MediaLibrary.getAssetsAsync({ mediaType: "audio" });
+    setMusics(foundMusics);
+  }
+
+  useEffect(() => {
+    getMusics();
+  });
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.content}>
-      <Text style={{fontSize:30, color:"#fff",textAlign:"center"}}>Tela Musicas Principal</Text>
+      <View style={styles.header}>
+                <TouchableOpacity>
+                    <FontAwesome5 name="search" size={29} color="white" />
+                </TouchableOpacity>
+                <Text style={styles.recentsTitleText}>Nome do App</Text>
+                <TouchableOpacity>
+                    <Ionicons name="settings-sharp" size={29} color="white" />
+                </TouchableOpacity>
+        </View>
+
+      <View>        
+        <FlatList
+          data = { musics?.assets }
+          renderItem ={ (infoItem) => <Music  
+              mode = "vertical"           
+              url={{ uri: "https://placecats.com/300/300" }} 
+              name = {infoItem.item.filename} 
+              key = { infoItem.item.id }
+              path = { infoItem.item.uri }
+              artist = "Desconhecido(a)"              
+            />          
+          }
+        />        
       </View>
     </SafeAreaView>
   );
@@ -14,9 +59,21 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
     backgroundColor: "#2A2727",
+    paddingTop: "5%",    
+    paddingBottom: "30%",
   },
-  content: {
-    flex: 1,
+  header: {
+    width: "90%",
+    marginTop: 15,
+    padding: 2,
+    marginBottom: "10%",
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  recentsTitleText: {
+    color: "#fff",
+    fontSize: 24,
   },
 });
